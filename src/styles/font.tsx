347f837454d164b9c5dd81725a9a3d8a25ed7ad2
aaminha <dks4857@gmail.com>
2024-11-04 21:40:07 +0900

    chore: 폰트 추가

--- src/styles/font.tsx
+++ src/styles/font.tsx
@@ -3,5 +3,9 @@ import localFont from 'next/font/local'
 export const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
   display: 'swap',
-  weight: '45 920',
+})
+
+export const helvetica = localFont({
+  src: '../../public/fonts/HelveticaInseratLTStd-Roman.woff2',
+  display: 'swap',
 })
