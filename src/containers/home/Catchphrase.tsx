48d229f9da96cc8cf40b8021576c1de2f604a051
aaminha <dks4857@gmail.com>
2024-11-09 19:39:19 +0900

    feat: 캐치프레이즈 섹션 임시 추가

---
+++ src/containers/home/Catchphrase.tsx
@@ -0,0 +1,9 @@
+export const Catchphrase = () => {
+  return (
+    <section className="h-[500px] bg-gray-90">
+      <div className="max-w-1440 flex h-full items-center justify-center">
+        <p>여기에는 전시에 대한 캐치프레이즈가 들어갑니다.</p>
+      </div>
+    </section>
+  )
+}
