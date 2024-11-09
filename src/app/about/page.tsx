24e5a53d5e1d062b13258f5305cca6ec1b21262d
aaminha <dks4857@gmail.com>
2024-11-09 11:25:34 +0900

    feat: About 페이지 구현

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import { Information } from '@src/containers/about/Information'
+import { Program } from '@src/containers/about/Program'
+import { Visuality } from '@src/containers/about/Visuality'
+
+export default function AboutPage() {
+  return (
+    <>
+      <Visuality />
+      <Information />
+      <Program />
+    </>
+  )
+}
