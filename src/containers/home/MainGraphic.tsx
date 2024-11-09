1ba92a1f2914b93903eba5e7091a66bd40cc1225
aaminha <dks4857@gmail.com>
2024-11-09 16:43:06 +0900

    design: MainGraphic 컴포넌트 디자인 수정

--- src/containers/home/MainGraphic.tsx
+++ src/containers/home/MainGraphic.tsx
@@ -1,12 +1,12 @@
 export const MainGraphic = () => {
   return (
-    <section className="relative bg-[#020202] py-[60px]">
+    <section className="relative bg-[#020202] py-20">
       <video
         src="/videos/exhibition.mp4"
         autoPlay
         muted
         loop
-        className="mx-auto max-h-[782px] w-full"
+        className="mx-auto mb-8 max-h-[782px] w-full"
       ></video>
       <div className="bg-home-gradient absolute bottom-0 flex h-[45%] w-full flex-col items-center justify-center">
         <button className="mb-3 mt-[30px] rounded-full bg-primary-2 px-10 py-4 text-b1 font-semibold text-white">
