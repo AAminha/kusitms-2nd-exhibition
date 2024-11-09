ddd04f05a5e3a1aa4dbb4e9fba26ec5bc8123654
aaminha <dks4857@gmail.com>
2024-11-09 19:41:22 +0900

    feat: 방문록 섹션 구현

---
+++ src/containers/home/GuestBookItem.tsx
@@ -0,0 +1,13 @@
+interface GuestBookItemProps {
+  content: string
+  date: string // Date 타입 변경 가능성
+}
+
+export const GuestBookItem = ({ content, date }: GuestBookItemProps) => {
+  return (
+    <article className="h-fit w-full shrink rounded-[12px] bg-gray-90 p-4 desktop:max-w-[365px]">
+      <p className="mb-2 break-all text-b4 font-medium text-gray-30">{content}</p>
+      <p className="float-end text-c1 font-normal text-gray-60">{date}</p>
+    </article>
+  )
+}
