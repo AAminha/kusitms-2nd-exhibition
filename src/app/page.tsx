dba217ae922b3d9fb1d41335da8e1c102497fddf
aaminha <dks4857@gmail.com>
2024-11-09 19:41:36 +0900

    feat: Home 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Footer } from '@src/components/Footer'
+import { Catchphrase } from '@src/containers/home/Catchphrase'
+import { GuestBook } from '@src/containers/home/GuestBook'
+import { Keyword } from '@src/containers/home/Keyword'
+import { MainGraphic } from '@src/containers/home/MainGraphic'
+
 export default function Home() {
-  return <div>홈</div>
+  return (
+    <>
+      <MainGraphic />
+      <Catchphrase />
+      <Keyword />
+      <GuestBook />
+      <Footer />
+    </>
+  )
 }
