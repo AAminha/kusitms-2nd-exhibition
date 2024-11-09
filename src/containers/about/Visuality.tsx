5ee04ef343cdfbb5ebc176960b11f62bbf120cb3
aaminha <dks4857@gmail.com>
2024-11-09 11:25:25 +0900

    fix: Program, Visuality 섹션 구조 수정

--- src/containers/about/Visuality.tsx
+++ src/containers/about/Visuality.tsx
@@ -1,21 +1,16 @@
 import clsx from 'clsx'
 
+import { Paragraph } from '@src/containers/about/Paragraph'
+import { SectionTitle } from '@src/containers/about/SectionTitle'
 import { helvetica } from '@src/styles/font'
 
 import { VISUALITY_INFORMATION } from './constant'
 
 export const Visuality = () => {
   return (
-    <>
-      <h2 className="text-h1 font-semibold text-gray-5 mobile:text-h2">
-        {VISUALITY_INFORMATION.title}
-      </h2>
-      <h3 className="text-b3 font-normal text-gray-30">{VISUALITY_INFORMATION.subtitle}</h3>
-      <article className="mb-16 mt-8 flex flex-col gap-6 text-b3 font-normal text-gray-20">
-        {VISUALITY_INFORMATION.descriptions.map((item, index) => (
-          <p key={index}>{item}</p>
-        ))}
-      </article>
+    <section className="mb-24">
+      <SectionTitle title={VISUALITY_INFORMATION.title} subtitle={VISUALITY_INFORMATION.subtitle} />
+      <Paragraph information={VISUALITY_INFORMATION.descriptions} className="mb-[64px] mt-8" />
       <section className="flex gap-3 mobile:flex-col desktop:h-max">
         {VISUALITY_INFORMATION.keywords.map((item) => (
           <article key={item.title} className="flex-1 rounded-lg bg-gray-90 p-4">
@@ -28,6 +23,6 @@ export const Visuality = () => {
           </article>
         ))}
       </section>
-    </>
+    </section>
   )
 }
