86fdf19a74d86fd993a4e0b01aa26009c481e7e0
Minha Ahn <dks4857@gmail.com>
2024-11-15 00:34:45 +0900

    Apply suggestions from code review
    design: 불필요한 배경색 제거

--- src/containers/about/Visuality.tsx
+++ src/containers/about/Visuality.tsx
@@ -9,7 +9,7 @@ import { helvetica } from '@src/styles/font'
 
 const Visuality = forwardRef<HTMLElement, unknown>((_, ref) => {
   return (
-    <section ref={ref} className="bg-red-400 py-[140px] mobile:py-[100px]">
+    <section ref={ref} className="py-[140px] mobile:py-[100px]">
       <SectionTitle title={VISUALITY_INFORMATION.title} subtitle={VISUALITY_INFORMATION.subtitle} />
       <Paragraph information={VISUALITY_INFORMATION.descriptions} className="mb-16 mt-8" />
       <section className="flex gap-3 mobile:flex-col desktop:h-max">
