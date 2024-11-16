1cb7c0877c32790c0eabfe9b42a71a1b770d3e57
aaminha <dks4857@gmail.com>
2024-11-16 22:33:28 +0900

    feat: Visuality 라이팅 반영

--- src/containers/about/Visuality.tsx
+++ src/containers/about/Visuality.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx'
 
 import { SectionTitle } from '@src/components/SectionTitle'
 import { VISUALITY_INFORMATION } from '@src/constants/about'
+import { EXHIBITION_KEYWORDS } from '@src/constants/home'
 import { Paragraph } from '@src/containers/about/Paragraph'
 import { helvetica } from '@src/styles/font'
 
@@ -13,14 +14,12 @@ const Visuality = forwardRef<HTMLElement, unknown>((_, ref) => {
       <SectionTitle title={VISUALITY_INFORMATION.title} subtitle={VISUALITY_INFORMATION.subtitle} />
       <Paragraph information={VISUALITY_INFORMATION.descriptions} className="mb-16 mt-8" />
       <section className="flex gap-3 mobile:flex-col desktop:h-max">
-        {VISUALITY_INFORMATION.keywords.map((item) => (
+        {EXHIBITION_KEYWORDS.map((item) => (
           <article key={item.title} className="flex-1 rounded-lg bg-gray-90 p-4">
             <h4 className={clsx('font-branding-b2 mb-2 text-gray-10', helvetica.className)}>
               {item.title}
             </h4>
-            <p className="whitespace-pre-wrap text-b4 font-normal text-gray-30">
-              {item.description}
-            </p>
+            <p className="text-b4 font-normal text-gray-30">{item.description}</p>
           </article>
         ))}
       </section>
