232dbbeac09afd421721e868410966c2dcf730a2
aaminha <dks4857@gmail.com>
2024-11-16 20:27:35 +0900

    fix: 푸터에 잘못된 후원사 로고 삭제

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { AsanLogo, CodeitLogo, DcampLogo, SangSangUnivLogo } from '@public/logos'
+import { AsanLogo } from '@public/logos'
 import KusitmLogo from '@public/logos/kusitms.svg'
 
 export const Footer = () => {
@@ -7,10 +7,7 @@ export const Footer = () => {
       <section className="max-w-1440 flex items-center justify-between px-20 py-[23px] mobile:w-fit mobile:flex-col mobile:gap-10">
         <KusitmLogo />
         <div className="flex w-fit items-center gap-[60px] mobile:flex-wrap mobile:justify-between mobile:gap-4">
-          <SangSangUnivLogo />
           <AsanLogo />
-          <DcampLogo />
-          <CodeitLogo />
         </div>
       </section>
     </footer>
