6fd5903fdd05437bd9f3cd1f17c44c81e22b893d
aaminha <dks4857@gmail.com>
2024-11-19 02:39:38 +0900

    feat: Card 컴포넌트 구현

---
+++ src/containers/archive/Card.tsx
@@ -0,0 +1,13 @@
+export const Card = () => {
+  return (
+    <article className="relative w-full">
+      <div className="h-[161px] rounded-[8px] bg-gray-80" />
+      <section className="mt-2 px-2">
+        <h2 className="text-b2 font-semibold text-gray-15">서비스명</h2>
+        <h3 className="text-b4 font-normal text-gray-40">
+          이 서비스는 이런 서비스입니다 이러쿵 저러쿵
+        </h3>
+      </section>
+    </article>
+  )
+}
