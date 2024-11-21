887f2a6340f20ea19b71cb8738182e1200eb0521
aaminha <dks4857@gmail.com>
2024-11-21 11:22:57 +0900

    feat: Archive 페이지 비공개 버전 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,13 +21,13 @@ export default function Home() {
   }
 
   return (
-    <>
+    <main>
       <Dialog open={openModal} onClose={handleClose} />
       <MainGraphic onOpen={handleOpen} />
       <Catchphrase />
       <Keyword />
       <GuestBook />
       <Footer />
-    </>
+    </main>
   )
 }
---
+++ src/containers/archive/CardSkeleton.tsx
@@ -0,0 +1,11 @@
+export const CardSkeleton = () => {
+  return (
+    <article className="relative w-full">
+      <div className="aspect-video rounded-[8px] bg-gray-80" />
+      <section className="mt-2 px-1">
+        <h2 className="mb-2 h-[20px] w-[35%] min-w-[90px] bg-gray-80 text-b2 font-semibold text-gray-15"></h2>
+        <h3 className="h-[20px] w-[70%] min-w-[100px] bg-gray-80 text-b4 font-normal text-gray-40"></h3>
+      </section>
+    </article>
+  )
+}
