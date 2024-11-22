268e5133f7f6c4472c1e89e94fb84da80408627d
aaminha <dks4857@gmail.com>
2024-11-22 23:37:28 +0900

    fix: Archive 미공개로 수정

---
+++ src/app/archive/_page.tsx
@@ -0,0 +1,11 @@
+import { Suspense } from 'react'
+
+import ArchivePageContent from '@src/containers/archive/PageContent'
+
+export default function ArchivePage() {
+  return (
+    <Suspense fallback={<p className="py-[140px] text-center mobile:py-[100px]">Loading...</p>}>
+      <ArchivePageContent />
+    </Suspense>
+  )
+}
