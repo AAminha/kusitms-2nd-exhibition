7257ab6f7fd0d1e278530d39a0563ece1af7a519
aaminha <dks4857@gmail.com>
2024-11-25 00:54:35 +0900

    feat: sitemap 생성

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.chemical-synergy-with-kusitms.com/',
+      lastModified: new Date('2024-11-25'),
+      changeFrequency: 'always',
+      priority: 1,
+    },
+    {
+      url: 'https://www.chemical-synergy-with-kusitms.com/about',
+      lastModified: new Date('2024-11-25'),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.chemical-synergy-with-kusitms.com/archive',
+      lastModified: new Date('2024-11-25'),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.chemical-synergy-with-kusitms.com/people',
+      lastModified: new Date('2024-11-25'),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ]
+}
