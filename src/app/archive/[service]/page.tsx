2c74615d7b3ee694d482451eda9c68417d006741
aaminha <dks4857@gmail.com>
2024-11-25 21:25:00 +0900

    feat: Archive 상세 페이지 구현

---
+++ src/app/archive/[service]/page.tsx
@@ -0,0 +1,19 @@
+import { Suspense } from 'react'
+
+import { Comment } from '@src/containers/detail/Comment'
+import { Service } from '@src/containers/detail/Service'
+import { Team } from '@src/containers/detail/Team'
+
+export default function ArchiveDetailPage({ params }: Readonly<{ params: { service: string } }>) {
+  const productId = params.service.split('-')[0]
+
+  return (
+    <main className="mx-auto flex max-w-[1168px] flex-col gap-20 pt-[140px]">
+      <Suspense fallback={<p>Loading...</p>}>
+        <Service productId={productId} />
+        <Team productId={productId} />
+        <Comment productId={productId} />
+      </Suspense>
+    </main>
+  )
+}
